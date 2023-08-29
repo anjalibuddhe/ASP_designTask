deb7653e09514516f86d7af713dc0cec19db54c9
anjali buddhe <anjalibuddhe124@gmail.com>
2023-08-29 09:39:43 +0530

    Add project files.

---
+++ Toysonus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Toysonus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
